8e3bb4c2b303638b2cd8a50d96dc0fa21d6dad5b
varadkinikar783 <varadkinikar783@gmail.com>
2026-04-21 08:52:53 +0530

    conditional expression

---
+++ conditional expression.py
@@ -0,0 +1,8 @@
+# conditional expressions are a shortcut to if-else statements
+# they are also known as ternary operators
+# examples are as follows:-
+X = 10
+print(("positive") if X > 0 else ("negative"))
+Y = 7
+print(("even") if Y % 2 == 0 else ("odd"))
+# in a way it's formula is like this:- X if condition else Y
