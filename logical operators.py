1e8165836a0689fca63969cc037f04ea395aeba6
varadkinikar783 <varadkinikar783@gmail.com>
2026-04-21 08:51:04 +0530

    Delete logical operators.py

--- logical operators.py
+++
@@ -1,6 +0,0 @@
-#logical operators are used to combine two or more conditions and give a single output(it's basically the three main logic gates)
-#the three main logic gates are 'and', 'or' and 'not'
-#'and' is used to join two statements and only gives output if both statements are true
-#'or' is used to join two statements and gives output if at least one of the statements is true
-#'not' is used to reverse the output of a statement(remember not gate)
-#the example of 'and' operator is given in temp and weight conversion program
